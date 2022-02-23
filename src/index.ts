7baaac5909e870d8a276c352ecf05d44ae8ae14f
tiagofcard <tiagof.cardoso@hotmail.com>
2022-02-23 20:00:39 -0300

    teste

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('./', (request: Request, response: Response) => {
+  return response.send('OK');
+})
+
+const port = process.env.PORT || 8080;
+
+app.listen(8080, () => {
+  console.log('listening')
+})
